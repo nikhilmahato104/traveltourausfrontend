65dfe1f688695e50a24414c9ca6633967e65ea37
nikhilmahato104 <nikhilmahato104@gmail.com>
2025-02-26 16:35:54 +0530

    add the lense png on footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import AdminPanel from "./pages/AdminPanel.jsx";
 import Navbar from "./components/Navbar.jsx"; // Import your Navbar component
-
+//app.css
+import "./app.css";
 const App = () => {
   return (
     <Router>
